dbcb1025428edf364c935ca34e2242814af19a34
KodaiD <bb_dkoudai@yahoo.co.jp>
2020-01-04 20:42:16 +0900

    add route

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+import "net/http"
+
 func main() {
-	fmt.Println("Chat App")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Simple server")
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
